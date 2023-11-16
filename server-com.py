bf8dba2cbd3e65b66c4e7cf9cfe3ef5dab8381d5
DESKTOP-UR56NLO\Admin <phuctoan0708@gmail.com>
2023-11-17 00:59:31 +0700

    multithread

--- server-com.py
+++ server-com.py
@@ -1,40 +1,6 @@
 import socket
 import threading
 
-def start_server():
-    server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    server_host = '0.0.0.0'
-    server_port = 12345
-
-    server_socket.bind((server_host, server_port))
-    server_socket.listen(1)
-    client_sockets = []
-    print(f"Server listening on {server_host}:{server_port}")
-
-    while True:
-        try: 
-            client_socket, client_address = server_socket.accept()
-            
-            print(f"Connection from {client_address}")
-            client_sockets.append(client_socket)
-
-            option = client_socket.recv(1024).decode("utf-8").strip()
-            print(option.upper())
-            
-            if option == "login":
-                if authenticate_client(client_socket):
-                    handle_client(client_socket)
-                else:
-                    print("Authentication failed. Closing connection.")
-                    client_socket.close()
-            elif option == "register":
-                user_register(client_socket)
-
-            # Start a new thread to handle the client
-            client_thread = threading.Thread(target=handle_client, args=(client_socket,))
-            client_thread.start()
-        except KeyboardInterrupt:
-            handle_server_exit(server_socket, client_sockets)
 
 def user_register(client_socket):
     username = client_socket.recv(1024).decode("utf-8").strip()
@@ -42,6 +8,7 @@ def user_register(client_socket):
 
     print(f"Received new username: {username}, new password: {password}")
 
+    user_acount_file = "users.txt"
     users_file_path = open("users.txt", "r")
     check = checkExitedAccount(username, password, users_file_path)
     users_file_path.close()
@@ -50,7 +17,7 @@ def user_register(client_socket):
         client_socket.send("FAILED".encode("utf-8"))
         return False 
     else: 
-        with open(users_file_path, "a") as file:
+        with open(user_acount_file, "a") as file:
             file.write(f"{username}:{password}\n")
         # client_socket.send("Registration successful. You can now log in.".encode())
         
@@ -58,13 +25,14 @@ def user_register(client_socket):
         print("Registration successful")
         return True
 
+
 def authenticate_client(client_socket):
     # Receive username and password separately
     username = client_socket.recv(1024).decode("utf-8").strip()
     password = client_socket.recv(1024).decode("utf-8").strip()
     
     # For debugging, print the received credentials
-    print(f"Received username: {username}, password: {password}")
+    #print(f"Received username: {username}, password: {password}")
 
     users_file = open("users.txt", "r")
     check = checkExitedAccount(username, password, users_file)
@@ -87,12 +55,6 @@ def checkExitedAccount(username, password, users_file):
 
     return False
 
-def handle_client(client_socket):
-    # Implement your file transfer logic here
-    receive_file(client_socket)
-
-    client_socket.close()
-
 def receive_file(client_socket):
     with open('received_file.txt', 'wb') as file:
         while True:
@@ -103,33 +65,43 @@ def receive_file(client_socket):
             print("File received successfully")
             client_socket.close()
 
+def handle_client(conn, addr):
+    print(f"[NEW CONNECTION] {addr} connected.")
 
-def handle_server_exit(server_socket, client_sockets):
-    print("Server is exiting. Closing all connections...")
-    for client_socket in client_sockets:
-        client_socket.close()
-    server_socket.close()
-    exit()
+    
+    connected = True
+    while connected:
+        option = conn.recv(1024).decode("utf-8").strip()
+        print(option.upper())
+        
+        if option == "login":
+            if authenticate_client(conn):
+                receive_file(conn)
+            else:
+                print("Authentication failed. Closing connection.")
+                conn.close()
+        elif option == "register":
+            user_register(conn)
+            conn.close()
+            connected = False
 
-
-
-
+    conn.close()
 
 def main():
     print("[STARTING] Server is starting...")
-    server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    server.bind(ADDR)
-    server.listen()
-    print(f"[LISTENING] Server is listening on {IP}:{PORT}")
+    server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server_host = '0.0.0.0'
+    server_port = 12345
+    server_socket.bind((server_host, server_port))
+    server_socket.listen(5)
+    print(f"Server listening on {server_host}:{server_port}")
 
     while True:
-        conn, addr = server.accept()
+        conn, addr = server_socket.accept()
         thread = threading.Thread(target=handle_client, args=(conn, addr))
         thread.start()
         print(f"[ACTIVE CONNECTIONS] {threading.activeCount() - 1}")
 
 
-
-
 if __name__ == "__main__":
-    start_server()
+    main()
