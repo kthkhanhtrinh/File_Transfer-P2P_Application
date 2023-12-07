5ceb0a9460ff629f75a88ec7bc3047f51df0e405
kthkhanhtrinh <khanhtrinh81103@gmail.com>
2023-12-07 11:22:11 +0700

    client final v1

---
+++ client_final.py
@@ -0,0 +1,171 @@
+import socket, os
+import threading
+data = None
+
+client_open_port = None
+# fetch_port = False
+file_sent = False
+
+def listen_from_another_client(): # C1 listen from C2
+    print("Port is opening, ready to connect peer!\n")
+    while True:
+        server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        server_socket.bind(('0.0.0.0', 12348))  # Bind to localhost and port 12345
+        server_socket.listen()
+
+
+        conn, addr = server_socket.accept()
+        print(f'Have a connection from {addr}')
+        data = conn.recv(1024).decode().strip()
+        print(data)
+        download_file(conn)
+        break
+        # with conn:
+        #     print(f"Connected by {addr}")
+        #     while True:
+        #         data = conn.recv(1024)
+        #         if not data:
+        #             break
+        #         conn.sendall(data)  # Echoes back the received data
+
+    print("Fetch port is close")
+
+
+
+def connect_to_another_client(ip, fname, line):  # C2 connect to C1
+    peer_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+    try:
+        peer_socket.connect((ip, 12348))  # Connect to the peer
+        # peer_send_file(peer_socket, line)
+        try: 
+            peer_socket.send('hello from another client'.encode())
+            peer_send_file(peer_socket, line)
+            # print("Send to peer success")
+        except: print("Send fail :((")
+    # data = client_socket.recv(1024)
+    except:
+        print("Connect to", ip, "failed")
+    # print(f'Received from another client: {data.decode()}')
+    
+
+def client_send2(s):
+    while True:
+        msg = input("Client command (publish/fetch): ")
+
+        if not (msg.startswith("publish") or msg.startswith("fetch")):
+            print("Error, enter again")
+            if msg.startswith("quit"):
+                s.close()
+            continue
+        
+        if msg.startswith("publish"):
+            #Check if valid file
+            _, lname, fname = msg.split(" ")
+            if check_valid_files(lname, fname):
+                msg = f"publish_{lname}_{fname}"
+            else:
+                print("Invalid lname or fname")
+                continue
+
+        if msg.startswith("fetch"):
+            _, fname = msg.split(" ")
+            msg = f"fetch_{fname}"
+            # try:
+            #     with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as u:
+                    
+            #         # msg = "Still coding please wait"
+            #         #def open port
+            #         # listen_from_another_client()
+
+            # except Exception:
+            #     print("Unable to get IP Address")
+
+        #if msg == "Sending":
+            #download_file(s)
+
+        s.sendall(msg.encode())
+        if msg.startswith("fetch"):
+            client_open_port = threading.Thread(target=listen_from_another_client, args=(),)
+            client_open_port.start()
+            # fetch_port = True
+            # threading.Thread(target=listen_from_another_client, args=(),).start()
+            # listen_from_another_client()
+
+
+def client_recv1(s):
+    while True:
+        data = s.recv(1024).decode().strip()
+        print("received data:", data)
+
+        if data.startswith("fetch fail"):
+            _, fname = data.split("_")
+            print(fname, "no such file founded!")
+        
+        elif data == "Sending":
+            # filename = s.recv(1024).decode().strip()
+            download_file(s)
+        elif data.startswith('fetch-request'):
+            _, client_ip, fname, line = data.split("_")
+            print(client_ip)
+            print(fname)
+            print(line)
+            threading.Thread(target=connect_to_another_client, args=(client_ip, fname, line)).start()
+            # connect_to_another_client(client_ip, fname, line)
+        
+        # Server send to fetch request from another client
+        else: 
+            continue
+
+def download_file(s):
+    # print("Writing")
+    try:
+        # with open("C:\\Users\\Admin\\Downloads\\received-files.txt", "wb") as file:
+        with open("C:\\Users\\khanh\\Downloads\\received-files.txt", "wb") as file:
+            data = s.recv(1024)
+            # print(data)
+            file.write(data)
+            # print("File received successfully")
+        file.close()
+        print("File download success")
+        # while(not file_sent):
+        #     pass
+        # fetch_port = False
+    except Exception as e:
+        print(f"Failed to open file: {e}")
+
+def peer_send_file(conn, line):
+    print("Sending")
+    try: conn.send("Sending".encode())
+    except: 
+        print(f"Send to {conn} fail")
+        return
+
+    # filename = line[line.rfind("\'") + 1:]
+    # # conn.send(filename.encode())
+    # filename = filename[:-1]
+    # print(filename)
+    with open(line, 'rb') as file:
+        data = file.read(1024)
+        # print(data)
+        while data:
+            conn.send(data)
+            data = file.read(1024)
+    file.close()
+    file_sent = True
+    print("Send file successful")              
+def peer_rev(s):
+    data = s.recv(1024).decode().strip()
+    if data == "Sending":
+        download_file(s)
+
+def check_valid_files(lname, fname):
+    full_path = os.path.join(lname, fname)
+    return os.path.isfile(full_path)
+
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    # s.connect(('10.128.158.79', 12345))
+    # s.connect(('localhost', 12345))
+    s.connect(('192.168.0.124', 12345))
+    threading.Thread(target=client_send2, args=(s,)).start()
+    client_recv1(s)
